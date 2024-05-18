26488fc12a462f9736ee79c6892037193224ed7b
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2024-05-18 15:14:05 +0300

    Session 4.1

---
+++ src/test/java/wait/DynamicLoadingTests.java
@@ -0,0 +1,22 @@
+package wait;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.DynamicLoadingPage;
+import pages.OnePage;
+
+import static org.testng.Assert.assertEquals;
+
+public class DynamicLoadingTests extends BaseTests {
+
+    @Test
+    public void testExampleOne() {
+        DynamicLoadingPage dynamicLoadingPage = homePage.clickOnDynamicLoadingLink();
+        OnePage onePage = dynamicLoadingPage.clickOnExampleOneLink();
+        onePage.clickOnStartButton();
+        String actualResult = onePage.getValidationMessage();
+        String expectedResult = "Hello World!";
+        assertEquals(actualResult, expectedResult);
+
+    }
+}
